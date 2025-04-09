2937046cf9201aff4b93c7f7004e8f57b51b7b05
Heitor Rodrigues Carvalho <heitorrodrigues2909@gmail.com>
2025-04-09 15:21:26 -0300

    add example using scanner

---
+++ programming-language/examples/src/ExampleUsingScanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ExampleUsingScanner {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Enter a number: ");
+    int number = scan.nextInt();
+
+    scan.close();
+
+    System.out.println("You entered: " + number);
+  }
+}

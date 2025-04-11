823a6bebc5b4fa9951d9765dea9839e652333099
Heitor Rodrigues Carvalho <heitorrodrigues2909@gmail.com>
2025-04-11 15:11:15 -0300

    add example using repetition

---
+++ programming-language/examples/src/ExampleUsingRepetition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ExampleUsingRepetition {
+  public static void main() {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Enter a number: ");
+
+    int num = scan.nextInt();
+
+    scan.close();
+
+    System.out.println("Numbers from 0 to " + num + ":");
+
+    for (int i = 0; i <= num; i++) {
+      System.out.print(i + " ");
+    }
+  }
+}
